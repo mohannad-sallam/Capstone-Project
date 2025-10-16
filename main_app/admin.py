95419a2ffc35572ed14b886c5641eb2dabfb5eb2
mohannad sallam <mohannadsallam7@gmail.com>
2025-10-16 05:14:50 +0300

    created the superuser and registered the models for the admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Parts, Profile, Review
+
+admin.site.register(Profile)
+admin.site.register(Parts)
+admin.site.register(Review)
+admin.site.register(Comment)
